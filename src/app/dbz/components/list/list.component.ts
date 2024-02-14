77829f73e5eabed00f9df807bf6224b7e328eea7
Diego Villalba <diego.villalba01@uptc.edu.co>
2024-02-13 20:10:14 -0500

    props padre a hijo

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-list',
   templateUrl: `./list.component.html`,
   styleUrl: './list.component.css',
 })
-export class ListComponent { }
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunk',
+    power: 5631
+  }]
+}
